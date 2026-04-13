9aa8da529d622e36442e3112383d2f58de11cc18
Jose Roberto Corona Ramirez <r-ocr@hotmail.com>
2026-04-13 10:26:28 -0700

    Add files via upload

---
+++ parcial-3-mvc/examen-practico/webappbasket/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Proyecto MVC de torneos de basket
+ * Alumno: José Roberto Corona Ramírez
+ * Archivo principal del sistema
+ */
+
+header("Location: views/admin/admin.php");
+exit;
+?>
